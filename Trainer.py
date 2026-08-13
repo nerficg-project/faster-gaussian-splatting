5934359d52164950c029fe8db2d3fb14a9b4ffb6
flo <flo.hahlbohm@gmail.com>
2026-08-13 16:08:18 +0200

    Fix training iteration callback docstring

--- Trainer.py
+++ Trainer.py
@@ -137,7 +137,7 @@ class FasterGS4DTrainer(GuiTrainer):
 
     @training_callback(priority=80)
     def training_iteration(self, iteration: int, dataset: 'BaseDataset') -> None:
-        """Performs a training step without actually doing the optimizer step."""
+        """Performs a training step."""
         # init modes
         self.model.train()
         dataset.train()
